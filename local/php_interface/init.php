f440ef93076227fa6e8e6febf84ab1f2736dc3eb
AleksejYagudin <49141634+AleksejYagudin@users.noreply.github.com>
2024-07-07 17:08:54 +0400

    first-commit

---
+++ local/php_interface/init.php
@@ -0,0 +1,36 @@
+<?php
+
+spl_autoload_register(function($sClassName)
+{
+    $sClassFile = __DIR__.'/classes';
+
+    if ( file_exists($sClassFile.'/'.str_replace('\\', '/', $sClassName).'.php') )
+    {
+        require_once($sClassFile.'/'.str_replace('\\', '/', $sClassName).'.php');
+        return;
+    }
+
+    $arClass = explode('\\', strtolower($sClassName));
+    foreach($arClass as $sPath )
+    {
+        $sClassFile .= '/'.ucfirst($sPath);
+    }
+
+    $sClassFile .= '.php';
+    if (file_exists($sClassFile))
+    {
+        require_once($sClassFile);
+    }
+});
+
+foreach( [
+             __DIR__.'/kernel.php',
+             __DIR__.'/events.php',
+             __DIR__.'/vendor/autoload.php',
+         ]
+         as $filePath ) {
+    if ( file_exists($filePath) )
+    {
+        require_once($filePath);
+    }
+}
