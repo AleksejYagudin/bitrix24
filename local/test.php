70421acc70470aceda8659fd9af2cac1c9ea5b58
AleksejYagudin <49141634+AleksejYagudin@users.noreply.github.com>
2024-08-04 18:58:27 +0400

    agents

--- local/test.php
+++ local/test.php
@@ -1,3 +1,15 @@
 <?php
+require_once($_SERVER["DOCUMENT_ROOT"] . "/bitrix/modules/main/include/prolog_before.php");
 
-echo 'test';
+use \Mib\Watcher\ActivityWatcher;
+use Bitrix\Main\Config\Option;
+
+CAgent::AddAgent(
+    ActivityWatcher::class . '::runAgent();',
+    'b24.academy',
+    'Y',
+    60,
+    '',
+    'Y',
+    \Bitrix\Main\Type\DateTime::createFromPhp(new DateTime('tomorrow 12am'))
+);
